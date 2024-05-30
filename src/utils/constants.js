b5bfc3b2036206b13314dc1f772db391bb302fe0
IsmartGaurav <freeguy5530@gmail.com>
2024-05-30 21:12:06 +0530

    fetch menu link updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
 export const LOGO_URL = "https://i.ibb.co/jRd0Fd0/logo.jpg";
-export const CART_URL = "https://img.icons8.com/skeuomorphism/32/experimental-shopping-cart-skeuomorphism.png";
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId="
-export const REST_URL = "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8947446&lng=75.8301169&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const CART_URL =
+  "https://img.icons8.com/skeuomorphism/32/experimental-shopping-cart-skeuomorphism.png";
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const MENU_URL =
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=";
+export const REST_URL =
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8947446&lng=75.8301169&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
