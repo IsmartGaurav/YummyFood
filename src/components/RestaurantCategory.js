32b13bc939d530312c5fd8f3077af833b6a1db46
Gaurav Sharma <98760746+IsmartGaurav@users.noreply.github.com>
2024-06-05 09:42:00 +0530

    Update RestaurantCategory.js

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,48 +1,29 @@
-import { CDN_URL } from "../utils/constants";
-import { useContext } from "react";
-import UserContext from "../utils/UseContext";
+import ItemList from "./ItemList";
 import React from "react";
 
-const Restaurants = (props) => {
-  const { loggedInUser } = useContext(UserContext);
-  const { resData } = props;
-  let { name, avgRating, cloudinaryImageId, sla, cuisines, areaName } =
-    resData?.info;
-  return (
-    <div
-      data-testid="resCard"
-      className="m-3 p-4 w-[280px] hover:scale-95 ease-in-out transition-transform duration-300"
-    >
-      <img
-        className="object-cover h-[150px] w-full rounded-lg"
-        src={CDN_URL + cloudinaryImageId}
-      />
-      <h3 className="ml-2 font-bold my-2 text-lg">{name}</h3>
+const RestuarantCategory = ({ data, showItems, showIndex }) => {
+  const handleItem = () => {
+    showIndex();
+  };
 
-      <p className="ml-2 text-base">
-        {cuisines.slice(0, 3).join(", ")}
-        {cuisines.length > 3 ? "..." : ""}
-      </p>
-      <h4 className="ml-2 text-base font-medium">
-        <div className="flex items-center">
-          <span className="mr-1">{avgRating}</span>
-          <img
-            className="inline-block w-4 h-4 mr-1"
-            src="https://res.cloudinary.com/dvovtfe0s/image/upload/v1717529992/star-7207_mvk2fo.svg"
-          />
-          • {sla.slaString}
-        </div>
-      </h4>
-      <div className="ml-2 text-base font-medium flex items-center">
-        <img
-          className="w-4 h-4"
-          src="https://res.cloudinary.com/dvovtfe0s/image/upload/v1717530548/fast-food-delivery-12982_jthlwk.svg"
-        />
-        <p className="mx-1">Distance {sla.lastMileTravelString} </p>
+  return (
+    <div className=" bg-zinc-100 w-7/12 mx-auto my-4 p-4 shadow-md ">
+      <div className="flex justify-between cursor-pointer">
+        <span className="font-bold text-2xl" onClick={handleItem}>
+          {data.title} ({data?.itemCards.length})
+        </span>
+        <span onClick={handleItem}>
+          {
+            <img
+              className="w-5 h-5"
+              src="https://res.cloudinary.com/dvovtfe0s/image/upload/v1717528206/chevron-down-solid_h2yqr2.svg"
+            />
+          }
+        </span>
       </div>
-      <p className="ml-2 text-base font-medium">Area : {areaName}</p>
+      {showItems && <ItemList itemsData={data?.itemCards} />}
     </div>
   );
 };
 
-export default Restaurants;
+export default RestuarantCategory;
